862fa84b1f69c330bb8f3226f7d2c8e1e0f1c02e
xnithunx <nithun.nanda@gmail.com>
2018-11-12 09:37:49 +0800

    completed deliverables

--- script.js
+++ script.js
@@ -1,34 +1,103 @@
-class List extends React.Component {
-  constructor(){
-    super()
-    this.changeHandler = this.changeHandler.bind( this );
-  }
+class ListInput extends React.Component {
 
-  state = {
-    list : [],
-    word : ""
-  }
+   constructor(){
+     super()
 
-  changeHandler(event){
-    this.setState({word:event.target.value});
-    console.log("change", event.target.value);
-  }
+     this.state ={
+     list: [],
+     word: ""
+     }
 
-  render() {
-      // render the list with a map() here
+     this.changeHandler = this.changeHandler.bind(this);
+     this.submitHandler = this.submitHandler.bind(this);
+     this.deleteHandler = this.deleteHandler.bind(this);
 
-      console.log("rendering");
-      return (
-        <div className="list">
-          <input onChange={this.changeHandler} value={this.state.word}/>
-          <button>add item</button>
-        </div>
-      );
-  }
-}
+   }
 
-ReactDOM.render(
-    <List/>,
-    document.getElementById('root')
-);
+   changeHandler(event){
 
+     if (event.target.value.length <= 15) {
+
+       this.setState({
+         word: event.target.value
+       });
+
+       console.log("change", event.target.value);
+
+     }
+
+     else {
+
+       alert('Entries must be less than 20 characters')
+
+     }
+
+   }
+
+   submitHandler(event){
+
+     this.setState({
+       word: "",
+       list: [...this.state.list, this.state.word]
+     })
+
+     event.preventDefault();
+
+   }
+
+   deleteHandler(index){
+
+     const list = this.state.list.filter((list, listIndex) => {
+       return listIndex !== index
+     })
+
+     this.setState({list})
+
+   }
+
+   render() {
+       // render the list with a map() here
+       console.log("rendering");
+
+       return (
+         <form onSubmit={this.submitHandler}>
+           <div className="todolist">
+             <h1><i>To-do List</i></h1>
+               <input onChange={this.changeHandler} value={this.state.word}/>
+               <button>Add</button>
+             <ShowList ToDoList={this.state.list} ToDelete={this.deleteHandler}/>
+           </div>
+         </form>
+       );
+
+   }
+
+ }
+
+ class ShowList extends React.Component {
+
+   render() {
+
+     return (
+       <div>
+       <br/>
+         <b> Current To-dos: </b>
+         <ul>
+          <div>
+           {this.props.ToDoList.map((List, index) =>
+             <li onClick={(event) => {this.props.ToDelete(index)}} key={index}> {List} </li>)}
+           </div>
+         </ul>
+       </div>
+     )
+
+   }
+
+ }
+
+ ReactDOM.render(
+
+     <ListInput/>,
+     document.getElementById('root')
+
+ );
